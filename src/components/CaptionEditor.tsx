3244e1bd3a2472e15410977c1b3c89adcd3f1bcb
yuichkun <yuichkun@gmail.com>
2022-09-16 14:09:23 -0700

    paint video on canvas

--- src/components/CaptionEditor.tsx
+++ src/components/CaptionEditor.tsx
@@ -1,6 +1,5 @@
-import { FC, useEffect, useRef, useState } from "react";
+import { FC, MouseEventHandler, useEffect, useRef, useState } from "react";
 
-// TODO: paint video inside canvas
 // TODO: place some content over the video
 // TODO: export the video
 // TODO: add seek bar
@@ -8,6 +7,7 @@ import { FC, useEffect, useRef, useState } from "react";
 export const CaptionEditor: FC = () => {
   const [videoFile, setVideoFile] = useState<File | null>(null);
   const videoElRef = useRef<HTMLVideoElement>(null);
+  const canvasElRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
     if (videoFile && videoElRef.current) {
@@ -19,6 +19,39 @@ export const CaptionEditor: FC = () => {
       };
     }
   }, [videoFile]);
+
+  useEffect(() => {
+    if (canvasElRef.current && videoElRef.current) {
+      videoElRef.current.addEventListener("loadedmetadata", function () {
+        if (!(canvasElRef.current && videoElRef.current)) return;
+        canvasElRef.current.width = videoElRef.current.videoWidth;
+        canvasElRef.current.height = videoElRef.current.videoHeight;
+      });
+
+      videoElRef.current.addEventListener("play", () => {
+        function step() {
+          if (!(canvasElRef.current && videoElRef.current)) return;
+          const ctx = canvasElRef.current.getContext("2d");
+          if (!ctx) throw new Error("2D context not available");
+          ctx.drawImage(
+            videoElRef.current,
+            0,
+            0,
+            canvasElRef.current.width,
+            canvasElRef.current.height
+          );
+          requestAnimationFrame(step);
+        }
+        requestAnimationFrame(step);
+      });
+    }
+  }, []);
+
+  const onPlay: MouseEventHandler<HTMLButtonElement> = () => {
+    if (videoElRef.current) {
+      videoElRef.current.play();
+    }
+  };
   return (
     <div>
       <h1 className="font-mono text-xl code">Awesome Caption Generator</h1>
@@ -31,17 +64,10 @@ export const CaptionEditor: FC = () => {
         onChange={(e) => setVideoFile(e.target.files![0])}
       />
       <div className="relative">
-        <canvas className="absolute w-full h-full" />
-        <video controls ref={videoElRef} />
+        <canvas ref={canvasElRef} />
+        <video className="hidden" controls ref={videoElRef} />
       </div>
-      <button
-        className="mt-8"
-        onClick={() => {
-          if (videoElRef.current) {
-            videoElRef.current.play();
-          }
-        }}
-      >
+      <button className="mt-8" onClick={onPlay}>
         Play
       </button>
     </div>
