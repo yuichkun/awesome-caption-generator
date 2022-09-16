a084dcfac390eab5193ca02a3a26284560ae0c7f
yuichkun <yuichkun@gmail.com>
2022-09-16 10:32:42 -0700

    add task memo

---
+++ components/CaptionEditor.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+
+// TODO: import video
+// TODO: show video
+// TODO: paint video inside canvas
+// TODO: place some content over the video
+// TODO: export the video
+// TODO: add seek bar
+
+export const CaptionEditor: FC = () => (
+  <div>
+    <h1 className="font-mono text-xl code">Awesome Caption Generator</h1>
+  </div>
+);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
+import { CaptionEditor } from "../../components/CaptionEditor";
+
 export default function Home() {
   return (
     <div className="container flex items-center p-4 mx-auto min-h-screen justify-center">
-      <main>
-        <h1 className="font-mono text-xl code">
-          Awesome Caption Generator
-        </h1>
-      </main>
+      <main>{<CaptionEditor />}</main>
     </div>
-  )
+  );
 }
