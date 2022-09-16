396827df5e03d88f5968a50bf673cb63cbc77a64
yuichkun <yuichkun@gmail.com>
2022-09-16 10:37:29 -0700

    import video

--- components/CaptionEditor.tsx
+++ components/CaptionEditor.tsx
@@ -1,14 +1,17 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
-// TODO: import video
 // TODO: show video
 // TODO: paint video inside canvas
 // TODO: place some content over the video
 // TODO: export the video
 // TODO: add seek bar
 
-export const CaptionEditor: FC = () => (
-  <div>
-    <h1 className="font-mono text-xl code">Awesome Caption Generator</h1>
-  </div>
-);
+export const CaptionEditor: FC = () => {
+  const [videoFile, setVideoFile] = useState<File | null>(null);
+  return (
+    <div>
+      <h1 className="font-mono text-xl code">Awesome Caption Generator</h1>
+      <input type="file" onChange={(e) => setVideoFile(e.target.files[0])} />
+    </div>
+  );
+};
