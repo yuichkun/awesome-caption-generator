744f34f0b73e161d73dc69076407de8e90294c50
yuichkun <yuichkun@gmail.com>
2022-09-16 10:17:17 -0700

    change title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="container flex items-center p-4 mx-auto min-h-screen justify-center">
       <main>
         <h1 className="font-mono text-xl code">
-          Welcome to <span className="text-purple-700">Nextjs</span>, <span className="text-indigo-700">TailwindCSS</span> and <span className="text-gray-700">TypeScript</span>
+          Awesome Caption Generator
         </h1>
       </main>
     </div>
